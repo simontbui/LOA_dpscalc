4e6dbf56d7918a10f44c9e472ae4daa85e769606
simontbui <simontb@uci.edu>
2022-06-23 15:53:57 -0700

    added list of inputs to output

--- app/routes.py
+++ app/routes.py
@@ -13,16 +13,23 @@ def index():
         #form = {'engravings': ['eng1', 'eng2', ---, ---, ...], 'lvl' = ['#', '#', ...],
         #        'crit-select': ['##' or ''], 'critdmg-select': ['##' or '']}
         form = request.form.to_dict(flat=False)
-        engrav_input = form["engravings"]
+        engrav_input = [x for x in form["engravings"] if x != '---']
         lvl_input = [int(x) for x in form["lvl"] if x != '---']
         crit_chance = [int(x) for x in form["crit-select"] if x is not None][0]
         crit_dmg  = [int(x) for x in form["critdmg-select"] if x is not None][0]
         multiplier = final_dps(engravings=engrav_input, engrav_lvl=lvl_input, c_rate=crit_chance,
                                 c_dmg=crit_dmg).multiplier
+
+        return render_template('index.html', engrav_list=engrav_list, multiplier=multiplier,
+                        crit_chance=crit_chance, crit_dmg=crit_dmg, engrav_input=engrav_input,
+                        lvl_input=lvl_input)
     else:
+        crit_chance = 0
+        crit_dmg = 200
         multiplier = 1
 
-    return render_template('index.html', engrav_list=engrav_list, multiplier=multiplier)
+        return render_template('index.html', engrav_list=engrav_list, multiplier=multiplier,
+                                crit_chance=crit_chance, crit_dmg=crit_dmg)
 
 # @app.route('/results', methods=['POST'])
 # def display_multiplier():
