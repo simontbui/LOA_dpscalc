60cfa58ee7d56d4432c838bb203034b2e3e944ab
simontbui <84045729+simontbui@users.noreply.github.com>
2022-06-28 21:22:05 -0700

    Delete app directory

--- app/selections.py
+++
@@ -1,7 +0,0 @@
-from wtforms import SelectField
-from flask_wtf import FlaskForm
-from app.engravings import engrav_list
-
-class Form(FlaskForm):
-    engravings = SelectField('Engravings', choices=engrav_list)
-    lvl = SelectField('Lvl', choices=[1,2,3])
